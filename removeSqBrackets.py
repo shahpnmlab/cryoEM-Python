c4e317ba9563172c70ab4571ece0a2d322a301ff
Pranav Shah <34034134+shahpnmlab@users.noreply.github.com>
2021-03-25 11:43:31 +0000

    Update removeSqBrackets.py

--- removeSqBrackets.py
+++ removeSqBrackets.py
@@ -1,28 +1,29 @@
 import os
-import glob
 
+import sys
 from pathlib import Path
-input_dir = "/path/to/frames/" # Change this based.
+
+input_dir = sys.argv[1]
 
 path_object = Path(input_dir)
 
 if path_object.is_dir() is False:
-     raise ValueError(f"Folder {path_object} does not exist.")
+    raise ValueError(f"Folder {path_object} does not exist.")
 
 mrc_files = list(path_object.glob("*.mrc"))
 xml_files = list(path_object.glob("*.xml"))
 
 renamed_mrc_files = []
 for m_file in mrc_files:
-    m_file = m_file.replace("[", "_")
-    m_file = m_file.replace("]", "")
-    renamed_mrc_files.append(files)
+    m_file = str(m_file).replace("[", "_")
+    m_file = str(m_file).replace("]", "")
+    renamed_mrc_files.append(m_file)
 
 renamed_xml_files = []
 for x_file in xml_files:
-    x_file = x_file.replace("[", "_")
-    x_file = x_file.replace("]", "")
-    renamed_xml_files.append(x_files)
+    x_file = str(x_file).replace("[", "_")
+    x_file = str(x_file).replace("]", "")
+    renamed_xml_files.append(x_file)
 
 print("REMOVING BRACKETS!")
 for m_old, n_old in zip(mrc_files, renamed_mrc_files):
